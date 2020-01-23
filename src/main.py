930787eb15ffb15334c539a3cdc7c309bed5c5cc
Martin Sørensen <maros105@gmail.com>
2020-01-23 11:59:25 +0100

    Spawn tweak

--- src/main.py
+++ src/main.py
@@ -127,16 +127,9 @@ def main():
         if not spawn.spawning:
             # Get the number of our creeps in the room.
             num_creeps = _.sum(Game.creeps, lambda c: c.pos.roomName == spawn.pos.roomName)
-            # If there are no creeps, spawn a creep once energy is at 250 or more
             if num_creeps <= 5 and spawn.room.energyAvailable >= 250:
                 spawn.createCreep([WORK, CARRY, MOVE, MOVE])
-            # If there are less than 10 creeps but at least one, wait until all spawns and extensions are full before
-            # spawning.
-            elif num_creeps < 10 and spawn.room.energyAvailable >= spawn.room.energyCapacityAvailable:
-                # If we have more energy, spawn a bigger creep.
-                if spawn.room.energyCapacityAvailable >= 350:
-                    spawn.createCreep([WORK, CARRY, CARRY, MOVE, MOVE, MOVE])
-            elif num_creeps == 0 and spawn.room.energyAvailable >= 250:
-                spawn.createCreep([WORK, CARRY, MOVE, MOVE])
+            elif num_creeps < 10 and spawn.room.energyCapacityAvailable >= 350:
+                spawn.createCreep([WORK, CARRY, CARRY, MOVE, MOVE, MOVE])
 
 module.exports.loop = main
